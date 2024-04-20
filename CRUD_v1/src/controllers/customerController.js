67ccecd7dbea14f8129acc90ac86e438564adeb7
william <wasuarezo30@gmail.com>
2024-04-19 21:59:22 -0500

    Version 2

--- CRUD_v1/src/controllers/customerController.js
+++ CRUD_v1/src/controllers/customerController.js
@@ -37,8 +37,7 @@ controller.delete = (req, res) => {
        });
      }
 
-
-     controller.edit = (req, res) => {
+   controller.edit = (req, res) => {
         const { id } = req.params;
         req.getConnection((err, conn) => {
         conn.query("SELECT * FROM cliente WHERE id = ?", [id], (err, rows) => {
@@ -49,20 +48,74 @@ controller.delete = (req, res) => {
    });
 };
 
-
 controller.update = (req, res) => {
     const { id } = req.params;
     const newCustomer = req.body;
     req.getConnection((err, conn) => {
-    
     conn.query('UPDATE cliente set ? where id = ?', [newCustomer, id], (err, rows) => {
     res.redirect('/');
     });
+  });
+};
+
+//*****************************************************************************/
+
+controller.listaProductos=(req,res)=>{
+    req.getConnection((err,conn)=>{
+        conn.query('SELECT * FROM producto',(err,customers)=>{
+            if(err){
+               res.json(err); 
+            }
+            console.log(customers);
+            res.render('productos_edit',{
+                data:customers
+            });
+        })
+
     });
+}
+
+controller.editarProductos = (req, res) => {
+        const { id } = req.params;
+            req.getConnection((err, conn) => {
+            conn.query("SELECT * FROM producto WHERE id = ?", [id], (err, rows) => {
+            res.render('productos', {
+            data: rows[0]
+        })
+      });
+    });
+}
+
+controller.saveProducto = (req, res) => {
+    const data = req.body;
+              //console.log(req.body);
+             // res.send('works');
+         req.getConnection((err, connection) => {
+         const query = connection.query('INSERT INTO producto set ?', data, (err, customer) => {
+         console.log(customer)
+         res.redirect('/productos');
+       });
+     });
+ }
+
+ controller.deleteProductos = (req, res) => {
+    const { id } = req.params;
+         req.getConnection((err, connection) => {
+         connection.query('DELETE FROM producto WHERE id = ?', [id], (err, rows) => {
+         res.redirect('/productos');
+        });
+       });
+     }
+
+     controller.updateProductos = (req, res) => {
+        const { id } = req.params;
+        const newCustomer = req.body;
+        req.getConnection((err, conn) => {
+        conn.query('UPDATE producto set ? where id = ?', [newCustomer, id], (err, rows) => {
+        res.redirect('/productos');
+        });
+      });
     };
-
-
-
 
 
 
--- CRUD_v1/src/routes/customer.js
+++ CRUD_v1/src/routes/customer.js
@@ -4,9 +4,15 @@ const router=express.Router();
 const customerController=require('../controllers/customerController');
 
 router.get('/',customerController.list);
-router.post('/add',customerController.save)
+router.post('/add',customerController.save);
 router.get('/delete/:id',customerController.delete);
 router.get('/update/:id', customerController.edit);
 router.post('/update/:id', customerController.update);
+
+router.get('/productos',customerController.listaProductos);
+router.post('/addProduct',customerController.saveProducto);
+router.get('/updateProductos/:id', customerController.editarProductos);
+router.post('/updateProductos/:id', customerController.updateProductos);
+router.get('/deleteProductos/:id',customerController.deleteProductos);
 
 module.exports=router;
